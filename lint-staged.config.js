071e5832756e21e6f53051e7256c7709bef8882f
mikasa <onlymisaky@gmail.com>
2024-11-12 17:45:19 +0800

    ci: 添加 lint-staged 支持

---
+++ lint-staged.config.js
@@ -0,0 +1,10 @@
+/**
+ * @link https://github.com/okonet/lint-staged
+ * @link https://blog.csdn.net/oschina_41559824/article/details/136965327
+ * @type {import('lint-staged').Config}
+ */
+export default {
+  '**/*.{js,jsx,ts,tsx,json}': ['npm run eslint'],
+  '**/*.{css,scss,sass,postcss}': ['npm run stylelint'],
+  '**/*.{vue}': ['npm run eslint && npm run stylelint'],
+};
