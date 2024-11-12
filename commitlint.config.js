b2f6ab0745f150832ae8af5e259ae6fc5322fd06
mikasa <onlymisaky@gmail.com>
2024-11-12 15:03:27 +0800

    ci: 添加 commitlinit 支持

---
+++ commitlint.config.js
@@ -0,0 +1,8 @@
+/**
+ * @link https://commitlint.js.org/
+ * @type {import('commitlint').UserConfig}
+ */
+export default {
+  extends: ['@commitlint/config-angular'],
+  // ignores: [(commit) => commit.includes('init')],
+};
