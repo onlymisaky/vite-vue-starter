a4f82dfca08d568943270bdc58bcf03b98f5c0ad
mikasa <onlymisaky@gmail.com>
2024-12-25 23:54:49 +0800

    refactor: migrate commitlint configuration from JS to TS

--- commitlint.config.js
+++
@@ -1,8 +0,0 @@
-/**
- * @link https://commitlint.js.org/
- * @type {import('commitlint').UserConfig}
- */
-export default {
-  extends: ['@commitlint/config-angular'],
-  // ignores: [(commit) => commit.includes('init')],
-};
