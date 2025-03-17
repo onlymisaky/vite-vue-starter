197e5be52698f38326fe20e7af95439a5443b39c
mikasa <onlymisaky@gmail.com>
2025-03-17 18:17:53 +0800

    feat: 增加标签页拖拽排序功能

--- src/layout/components/ViewTab/ViewTab.vue
+++ src/layout/components/ViewTab/ViewTab.vue
@@ -1,12 +1,57 @@
 <script lang="ts" setup>
 import type { IViewTab } from '@/store/modules/view-tab';
 import { useViewTabStore } from '@/store/modules/view-tab';
+import { ref } from 'vue';
 
 const viewTab = useViewTabStore();
 
 function removeTab(tab: IViewTab, event: Event) {
   event.stopPropagation();
   viewTab.removeTab(tab.fullPath);
+}
+
+function handleContextMenu(event: MouseEvent, _tab: IViewTab, _index: number) {
+  event.stopPropagation();
+  event.preventDefault();
+}
+
+const dragIndex = ref<number>(-1);
+const moving = ref(false);
+
+function handleDragStart(event: DragEvent, _tab: IViewTab, _index: number) {
+  event.stopPropagation();
+  dragIndex.value = _index;
+}
+
+function handleDragEnter(event: DragEvent, _tab: IViewTab, _index: number) {
+  event.stopPropagation();
+  if (moving.value) {
+    return;
+  }
+  const temp = viewTab.tabs[_index];
+  viewTab.tabs[_index] = viewTab.tabs[dragIndex.value];
+  viewTab.tabs[dragIndex.value] = temp;
+  dragIndex.value = _index;
+}
+
+function handleDragOver(event: DragEvent, _tab: IViewTab, _index: number) {
+  event.preventDefault();
+}
+
+function handleDragEnd(event: DragEvent, _tab: IViewTab, _index: number) {
+  event.stopPropagation();
+  event.preventDefault();
+  moving.value = false;
+}
+
+function handleTransitionStart(event: TransitionEvent, _tab: IViewTab, _index: number) {
+  event.stopPropagation();
+  moving.value = true;
+}
+
+function handleTransitionEnd(event: TransitionEvent, _tab: IViewTab, _index: number) {
+  event.stopPropagation();
+  moving.value = false;
 }
 </script>
 
@@ -16,7 +61,11 @@ function removeTab(tab: IViewTab, event: Event) {
     class="views-tab-wrapper"
   >
     <ElScrollbar class="flex-1">
-      <div class="views-tab">
+      <TransitionGroup
+        name="drag"
+        tag="div"
+        class="views-tab"
+      >
         <div
           v-for="(tab, index) in viewTab.tabs"
           :key="tab.fullPath"
@@ -24,6 +73,13 @@ function removeTab(tab: IViewTab, event: Event) {
           class="views-tab-item group"
           draggable="true"
           @click="viewTab.setActive(tab)"
+          @contextmenu="handleContextMenu($event, tab, index)"
+          @dragstart="handleDragStart($event, tab, index)"
+          @dragenter="handleDragEnter($event, tab, index)"
+          @dragover="handleDragOver($event, tab, index)"
+          @dragend="handleDragEnd($event, tab, index)"
+          @transitionstart="handleTransitionStart($event, tab, index)"
+          @transitionend="handleTransitionEnd($event, tab, index)"
         >
           <span
             class="divider group-hover:invisible"
@@ -49,7 +105,7 @@ function removeTab(tab: IViewTab, event: Event) {
             <Close />
           </ElIcon>
         </div>
-      </div>
+      </TransitionGroup>
     </ElScrollbar>
   </div>
 </template>
@@ -73,11 +129,20 @@ html.dark {
 </style>
 
 <style scoped>
+.drag-move,
+.drag-enter-active,
+.drag-leave-active {
+  transition: transform 0.3s ease;
+}
+
+:deep(.el-scrollbar__bar.is-vertical) {
+  width: 0 !important;
+  height: 0 !important;
+}
+
 .views-tab-wrapper {
   display: flex;
   background-color: var(--chrome-tab-bg-color);
-
-  /* #e5e7eb  */
   border-bottom: 1px solid var(--chrome-tab-border-color);
 
   .views-tab {
@@ -87,8 +152,6 @@ html.dark {
     align-items: center;
     height: 38px;
     padding-top: 4px;
-
-    /* overflow-x: auto; */
 
     .views-tab-item {
       height: 100%;
@@ -149,6 +212,16 @@ html.dark {
 
       &:hover:not(.active) {
         background-color: var(--chrome-tab-hover-bg-color);
+
+        &+.views-tab-item {
+          .divider {
+            visibility: hidden;
+          }
+        }
+      }
+
+      &:active:not(.active) {
+        background-color: var(--chrome-tab-active-bg-color);
 
         &+.views-tab-item {
           .divider {
