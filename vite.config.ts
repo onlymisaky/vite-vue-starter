bef31960b58b54b8fc88d4acaf0f78ead0732456
mikasa <onlymisaky@gmail.com>
2025-03-26 15:39:04 +0800

    fix: 修复幽灵依赖 vite-plugin-vue-inspector

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,9 @@ import AutoImport from 'unplugin-auto-import/vite';
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 import Components from 'unplugin-vue-components/vite';
 import { defineConfig, loadEnv } from 'vite';
+// import Inspect from 'vite-plugin-inspect';
 import vueDevTools from 'vite-plugin-vue-devtools';
-import vueInspector from 'vite-plugin-vue-inspector';
+// import vueInspector from 'vite-plugin-vue-inspector';
 import { dependencies } from './package.json';
 
 const skip = ['nprogress', 'vue'];
@@ -36,7 +37,8 @@ export default defineConfig((config) => {
     plugins: [
       vue(),
       vueDevTools(),
-      vueInspector(),
+      // Inspect(),
+      // vueInspector(),
       codeInspectorPlugin({ bundler: 'vite' }),
       legacy({
         targets: ['defaults', 'not IE 11'],
