70b64838c51ecc951d20067bd7da0db2b43c7b64
mikasa <onlymisaky@gmail.com>
2025-05-23 22:41:50 +0800

    fix: 修复 ConfigProviderProps 导入问题并调整类型定义

--- src/store/modules/el-config.ts
+++ src/store/modules/el-config.ts
@@ -1,8 +1,27 @@
-import type { ConfigProviderProps } from 'element-plus';
+import type { ConfigProviderProps as ConfigProviderProps2 } from 'element-plus';
 import zhCn from 'element-plus/es/locale/lang/zh-cn';
 import { defineStore } from 'pinia';
 import { ref } from 'vue';
 import store from '@/store';
+
+/**
+ * 如果直接使用 ConfigProviderProps 会导致构建报错
+ * 导出的变量“useElConfigStore”具有或正在使用外部模块“"node_modules/element-plus/es/index"”中的名称“TranslatePair”，但不能为其命名。
+ * 就很突然很莫名奇妙的出现了这个错误
+ * 我不想找原因了，很烦，脖子疼
+ */
+interface TranslatePair {
+  [key: string]: string | string[] | TranslatePair
+}
+
+type ConfigProviderProps = Omit<ConfigProviderProps2, 'locale'> & {
+  locale: {
+    name: string
+    el: {
+      [key: string]: TranslatePair
+    }
+  }
+};
 
 const defaultConfig: Partial<ConfigProviderProps> = {
   // 如果想重置 el-plus 的默认配置，可以在这里覆盖
