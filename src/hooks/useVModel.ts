0c50045addbf91458e5e5459342fe90e282f6558
mikasa <onlymisaky@gmail.com>
2025-09-20 13:46:58 +0800

    feat: 优化 useVModel ，支持对象类型的 modelValue 属性修改

--- src/hooks/useVModel.ts
+++ src/hooks/useVModel.ts
@@ -1,6 +1,29 @@
 import type { ComponentInternalInstance } from 'vue';
 import { computed, getCurrentInstance } from 'vue';
 
+function getProxyVal<P extends object, K extends keyof P>(
+  props: Readonly<P>,
+  key: K,
+  setter: (v: P[K]) => void,
+): P[K] {
+  const val = props[key];
+  const valType = Object.prototype.toString.call(val).slice(8, -1);
+  if (val && typeof val === 'object') {
+    if (['Object', 'Array'].includes(valType)) {
+      return new Proxy(val, {
+        get(target, k, receiver) {
+          return Reflect.get(target, k, receiver);
+        },
+        set(target, k, v) {
+          setter({ ...target, [k]: v } as P[K]);
+          return true;
+        },
+      });
+    }
+  }
+  return val;
+}
+
 export function useVModel<
   P extends object,
   K extends keyof P,
@@ -15,12 +38,16 @@ export function useVModel<
   const $emit = emit || instance?.emit;
   const event = `update:${key as string}` as EventName;
 
+  const setter = (v: P[K]) => {
+    $emit(event, v);
+  };
+
   return computed<P[K]>({
     get() {
-      return props[key];
+      return getProxyVal(props, key, setter);
     },
     set(v: P[K]) {
-      $emit(event, v);
+      setter(v);
     },
   });
 }
