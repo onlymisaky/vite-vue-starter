6f51e487150a8b5eb7b818f0f71f73de63d6e167
mikasa <onlymisaky@gmail.com>
2026-02-04 18:20:30 +0800

    fix(lint-staged): 修正类型注释为Configuration

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,7 @@
 /**
  * @link https://github.com/okonet/lint-staged
  * @link https://blog.csdn.net/oschina_41559824/article/details/136965327
- * @type {import('lint-staged').Config}
+ * @type {import('lint-staged').Configuration}
  */
 export default {
   // '**/*.{js,jsx,ts,tsx,json}': ['npm run eslint'],
